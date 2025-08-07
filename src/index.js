7deb6166b21f5c9e9bc8a83ee4c2cae585ee56e8
Sam Thool <samthool@outlook.com>
2025-08-07 18:03:13 +0530

    heysh

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(
   })
 );
 app.use(express.json({ limit: "50mb" }));
-app.options("*", cors());
+// app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
 
 // ✅ Static Files & Routes
